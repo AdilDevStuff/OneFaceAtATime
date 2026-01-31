4e84e09ed4c51219fa7857fa873b0a7983574c23
AdilDevStuff <adilshafiq784@gmail.com>
2026-01-31 17:40:36 +0500

    Radial Menu

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,8 +1,32 @@
 extends Control
 class_name GameUI
 
+@export var none_texture: Texture
+@export var red_texture: Texture
+@export var blue_texture: Texture
+@export var green_texture: Texture
+
 @export var current_active_mask_label: Label
 @export var game_manager: GameManager
+@export var radial_menu: RadialMenu
+
+func _ready() -> void:
+	radial_menu.set_items([])
+	radial_menu.add_icon_item(none_texture, "None", 0)
+	radial_menu.add_icon_item(red_texture, "Red", 1)
+	radial_menu.add_icon_item(blue_texture, "Blue", 2)
+	radial_menu.add_icon_item(green_texture, "Green", 3)
 
 func _process(_delta: float) -> void:
 	current_active_mask_label.text = "Mask: %s" % Globals.Masks.keys()[Globals.current_mask]
+	
+	if Input.is_action_pressed("open_radial"):
+		Engine.time_scale = 0.2
+		radial_menu.open_menu(get_viewport_rect().size / 2)
+	#elif Input.is_action_just_released("open_radial"):
+		#radial_menu.close_menu()
+
+func _on_radial_menu_item_selected(id: Variant, position: Variant) -> void:
+	get_tree().call_group("switching", "switch_mask", id)
+	#Globals.current_mask = id
+	Engine.time_scale = 1.0
